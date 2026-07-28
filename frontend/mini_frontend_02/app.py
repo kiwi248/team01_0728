c7fec007b6b415792a8eab1e6a32a7f960ca9a0a
jasnok <jso46034@naver.com>
2026-07-28 17:03:38 +0900

    작업 내용 백업

--- frontend/mini_frontend_02/app.py
+++ frontend/mini_frontend_02/app.py
@@ -28,15 +28,13 @@ if st.session_state.loginout != stored_loginout:
 home_page = st.Page("app_pages/01_home.py", title="홈", icon="🏠", default=True)
 login_page = st.Page("app_pages/00_login.py", title="로그인", icon="🔐")
 signup_page = st.Page("app_pages/02_signup.py", title="회원가입", icon="📝")
-weather_page = st.Page("app_pages/03_weather.py", title="날씨조회", icon="🌥️")
-health_page = st.Page("app_pages/04_health.py", title="서버체크", icon="❤️‍🩹")
+health_page = st.Page("app_pages/03_health.py", title="서버체크", icon="❤️‍🩹")
 
 
 
 if st.session_state.loginout == "login":
     pages = [
         home_page,
-        weather_page
     ]
 else:
     pages = [home_page, login_page, signup_page, health_page]
@@ -49,7 +47,6 @@ with st.sidebar:
 
     if st.session_state.loginout == "login":
         st.button("LOGOUT", on_click=logout, use_container_width=True)
-        st.page_link(weather_page)
     else:
         st.page_link(login_page)
         st.page_link(signup_page)
